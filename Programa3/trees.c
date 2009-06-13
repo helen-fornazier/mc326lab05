277852b814bace43e2bb589417738be0714eb6db
m063244@yahoo.com.br <m063244@yahoo.com.br@b37c2a90-5850-11de-b31f-81d54249e7d9>
2009-06-13 21:07:33 +0000

    correção da funcao le_chaves

--- Programa3/trees.c
+++ Programa3/trees.c
@@ -12,9 +12,13 @@ void pre_tree(FILE *entrada,FILE *destino,FILE *desprezados,int ind_chave,campos
     noh busc_noh; 
     noh *reg;
     char *reg_completo = malloc(sizeof(char)*ftam(campo,n_campos));
+    
+    /*abertura abertura */
+    
+    
     fgetc(entrada);
     while(! eof(entrada)){
-            seek(entrada,-1,SEEK_CUR);
+            fseek(entrada,-1,SEEK_CUR);
             /*MIRIA E RUBENS: le_chave*/  
             reg = le_chave(entrada,reg_completo,campo,n_campos); 
             /*MIRIA E RUBENS: busca_noh*/  
@@ -64,21 +68,30 @@ noh busca_noh(FILE *destino,noh *reg){
 }
 noh *le_chave(FILE *entrada, char *reg_completo, campos *campo, int n_campos){
     noh *novos_dados;
+//    int tam = ftam(campos *campo,int n_campos);
     int tam = ftam(campo,n_campos);
-    int ind = ret_indice_chave(campos *campo,int n_campos);
+//    int ind = ret_indice_chave(campos *campo,int n_campos);
+    int ind = ret_indice_chave(campo,n_campos);
     int chave_tam = campo[ind-1].pf - campo[ind-1].pi + 1;
     char *chave = (char*) malloc (sizeof(char)*(chave_tam+1));
     /*gravação do valor do inicio do registro na variável a ser retornada*/
-    novos_dados.end_noh = ftell (entrada);
+//    novos_dados.end_noh = ftell (entrada);
+    novos_dados->end_noh = ftell (entrada);
     /*gravação do registro completo em reg_completo*/
-    fread (reg_completo,sizeof(char),tam,fileptr);
+//    fread (reg_completo,sizeof(char),tam,fileptr);
+    fread (reg_completo,sizeof(char),tam,entrada);
     reg_completo[tam] = '\0';
     /*reposicionamento do ponteiro de arquivo no inicio do registro*/
     fseek (entrada, -tam, SEEK_CUR);
     fseek (entrada, campo[ind-1].pi, SEEK_CUR);
     /*gravação do valor da chave na variável a ser retornada*/
-    fread (chave,sizeof(char),chave_tam,fileptr);
+//  fread (chave,sizeof(char),chave_tam,fileptr);
+    fread (chave,sizeof(char),chave_tam,entrada);
     chave[chave_tam] = '\0';
-    novos_dados.valor = atoi (chave);
+//  novos_dados.valor = atoi (chave);
+    novos_dados->valor = atoi (chave);
     return novos_dados;
 }
+
+void grava_reg_desp(FILE *desprezados,char *reg_completo){
+     
