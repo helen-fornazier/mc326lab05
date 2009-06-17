a189eb780a54098d017be86160bcf002c2213c98
terra0009 <terra0009@b37c2a90-5850-11de-b31f-81d54249e7d9>
2009-06-17 16:25:45 +0000

    refeito pirate.c

--- trunk/pirate.c
+++ trunk/pirate.c
@@ -26,7 +26,9 @@ pagina new_page(){
 
 void escreve_pag(FILE *f, long int ad, pagina pag){
 	int i;
-	
+
+	fseek(f,ad,SEEK_SET);
+
 	fwrite(&pag.ap_pai,sizeof(long int),1,f);
 	for(i=0;i<9;i++){
 		fprintf(f," ");
@@ -45,6 +47,8 @@ pagina le_pag(FILE *f, long int ad){
 	int i;
 	pagina pag;
 	
+	fseek(f,ad,SEEK_SET);
+
 	fread(&pag.ap_pai,sizeof(long int),1,f);
 	fseek(f,1,SEEK_CUR);
 	for(i=0;i<9;i++){
@@ -60,6 +64,8 @@ pagina le_pag(FILE *f, long int ad){
 	return pag;
 }
 
+/*Funcao acha espaco no final de arquivo, devolve endereco*/
+
 long int newpagespace(FILE *f){
 	long int n;
 	char a='\n';
@@ -73,6 +79,14 @@ long int newpagespace(FILE *f){
 	return n;
 }
 
+/*Funcão adiciona um noh em uma página, parametros
+ * f-> o arquivo
+ * *root-> ponteiro para o endereço da raiz
+ * reg-> o nó a ser adicionado
+ * ord - a ordem da árvore
+ * ad - o endereço da pagina
+ * adright - o endereço da página que fica a direira do reg*/
+
 void add_node(FILE *f, long int *root, noh reg, int ord, long int ad, long int adright){
 	pagina pag;
 	noh tempnoh, tempnoh2;
@@ -83,6 +97,7 @@ void add_node(FILE *f, long int *root, noh reg, int ord, long int ad, long int a
 	for(i=0;i<ord;i++)
 		if(pag.n[i].valor==-1) tempint=1;
 	
+/* se tiver espaço, apenas insere o noh */
 	if(tempint){
 		for(i=0;i<ord;i++){
 			if(reg.valor<pag.n[i].valor){
@@ -95,8 +110,10 @@ void add_node(FILE *f, long int *root, noh reg, int ord, long int ad, long int a
 			}
 		}
 	}
-	
+
+/*caso contrário*/
 	else{
+/*Se a página não tem pai, cria ele*/
 		pagina newpag;
 		if(pag.ap_pai==-1){
 			newpag=new_page();
@@ -106,7 +123,7 @@ void add_node(FILE *f, long int *root, noh reg, int ord, long int ad, long int a
 			pag.ap_pai=*root;
 		}
 		
-		
+/* divide a página em duas e vê onde fica o novo nó */		
 		tempint=(ord/2)+1;
 		newpag=new_page();
 		for(i=0,j=0;i<ord;i++){
@@ -137,23 +154,26 @@ void add_node(FILE *f, long int *root, noh reg, int ord, long int ad, long int a
 		newpag.f[j]=pag.f[i];
 		pag.f[i]=-1;	
 		newpag.ap_pai=pag.ap_pai;
-		
+	
+/* escreve o novo nó */
 		templint=newpagespace(f);
 		escreve_pag(f,templint,newpag);
 
+/*modifica o pai de todos os filhos do novo nó*/
 		for(i=0;newpag.f[i]!=-1;i++){
 			pag=le_pag(f,newpag.f[i]);
 			pag.ap_pai=templint;
 			escreve_pag(f,newpag.f[i],pag);
 		}
 
+/* insere o elemento que subiu na página pai */
 		add_node(f,root,tempnoh2,ord,newpag.ap_pai,templint);
 	
 	}
 
 }
 
-
+/* insere um nó na arvore */
 void adiciona_na_tree(FILE *f, long int *root, noh reg, int ord){
 	noh temp;
 
